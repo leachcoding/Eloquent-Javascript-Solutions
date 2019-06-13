c0984e5ed7d769a6978bab09ee5e7c4d0cb7031d
Jay Leach <42565053+leachcoding@users.noreply.github.com>
2019-06-13 19:10:51 -0400

    Create recursion.js

---
+++ recursion.js
@@ -0,0 +1,15 @@
+// Define a recursive function isEven corresponding to this description. The function should accept a single parameter (a positive, whole number) and return a Boolean.
+
+function isEven(n) {
+  if (n == 0) return true;
+  else if (n == 1) return false;
+  else if (n < 0) return isEven(-n);
+  else return isEven(n - 2);
+}
+
+console.log(isEven(50));
+// → true
+console.log(isEven(75));
+// → false
+console.log(isEven(-1));
+// → false
